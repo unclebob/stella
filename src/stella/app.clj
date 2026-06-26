2b2119e234aaa3f79360ffc1f94d341d2c1ea451
Robert C. Martin <unclebob@cleancoder.com>
2026-06-26 14:45:44 -0500

    Clean up CljFX shell for architectural review
    Extract testable event routing from app startup, drive menu descriptions
    from declarative specs, wire CRAP/DRY/mutation tooling, and add app tests
    so all functions score CRAP 6 or below.

--- src/stella/app.clj
+++ src/stella/app.clj
@@ -4,6 +4,22 @@
   (:import [javafx.application Platform]
            [javafx.scene.control Alert Alert$AlertType]))
 
+(defn app-event-action
+  "Maps a CljFX event to an application action keyword, or nil when ignored."
+  [event]
+  (case (:fx/event-type event)
+    :stella.app/quit :quit
+    :stella.app/show-about :show-about
+    nil))
+
+(defn effect-for-action
+  "Maps an application action to an effect keyword executed on the JavaFX thread."
+  [action]
+  (case action
+    :quit :platform-exit
+    :show-about :about-dialog
+    nil))
+
 (defn- about-dialog []
   (doto (Alert. Alert$AlertType/INFORMATION)
     (.setTitle "About Stella")
@@ -11,12 +27,19 @@
     (.setContentText "A system dynamics diagram editor.")
     .showAndWait))
 
+(def ^:private effect-runners
+  {:platform-exit #(Platform/exit)
+   :about-dialog about-dialog})
+
+(defn- run-app-effect [effect]
+  (when-let [run (get effect-runners effect)]
+    (run)))
+
+(defn- handle-app-event [event]
+  (-> event app-event-action effect-for-action run-app-effect))
+
 (defn start!
   []
   (fx/create-app
    {:desc-fn (fn [_] (root/root-desc))
-    :event-handler (fn [event]
-                     (case (:fx/event-type event)
-                       :stella.app/quit (Platform/exit)
-                       :stella.app/show-about (about-dialog)
-                       nil))}))
+    :event-handler handle-app-event}))
--- src/stella/ui/menu.clj
+++ src/stella/ui/menu.clj
@@ -1,40 +1,40 @@
 (ns stella.ui.menu)
 
-(defn- menu-item
-  ([text] (menu-item text true nil))
-  ([text disabled] (menu-item text disabled nil))
-  ([text disabled on-action]
-   (cond-> {:fx/type :menu-item :text text}
-     disabled (assoc :disable true)
-     on-action (assoc :on-action on-action))))
+(def ^:private menu-specs
+  [{:label "File"
+    :items ["New" "Open…" "Save" "Save As…" :separator
+            {:text "Quit" :enabled true :on-action {:event :stella.app/quit}}]}
+   {:label "Edit"
+    :items ["Undo" "Redo" :separator "Cut" "Copy" "Paste"]}
+   {:label "View"
+    :items ["Zoom In" "Zoom Out" "Reset Zoom"]}
+   {:label "Help"
+    :items [{:text "About Stella"
+             :enabled true
+             :on-action {:event :stella.app/show-about}}]}])
 
 (defn- separator []
   {:fx/type :separator})
 
+(defn- menu-item-desc
+  [{:keys [text enabled on-action]
+    :or {enabled false}}]
+  (cond-> {:fx/type :menu-item :text text}
+    (not enabled) (assoc :disable true)
+    on-action (assoc :on-action on-action)))
+
+(defn- item-desc [spec]
+  (cond
+    (= :separator spec) (separator)
+    (string? spec) (menu-item-desc {:text spec})
+    (map? spec) (menu-item-desc spec)))
+
+(defn- menu-desc [{:keys [label items]}]
+  {:fx/type :menu
+   :text label
+   :items (mapv item-desc items)})
+
 (defn menu-bar-desc
   []
   {:fx/type :menu-bar
-   :menus [{:fx/type :menu
-            :text "File"
-            :items [(menu-item "New")
-                    (menu-item "Open…")
-                    (menu-item "Save")
-                    (menu-item "Save As…")
-                    (separator)
-                    (menu-item "Quit" false {:event :stella.app/quit})]}
-           {:fx/type :menu
-            :text "Edit"
-            :items [(menu-item "Undo")
-                    (menu-item "Redo")
-                    (separator)
-                    (menu-item "Cut")
-                    (menu-item "Copy")
-                    (menu-item "Paste")]}
-           {:fx/type :menu
-            :text "View"
-            :items [(menu-item "Zoom In")
-                    (menu-item "Zoom Out")
-                    (menu-item "Reset Zoom")]}
-           {:fx/type :menu
-            :text "Help"
-            :items [(menu-item "About Stella" false {:event :stella.app/show-about})]}]})
+   :menus (mapv menu-desc menu-specs)})
