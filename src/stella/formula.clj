ac4d76a257af8321a41427afe0f87e75c483262d
Robert C. Martin <unclebob@cleancoder.com>
2026-06-30 11:26:22 -0500

    Merge architect batch item ea9ea10a06: formula parser deduplication and function support

--- src/stella/formula.clj
+++ src/stella/formula.clj
@@ -2,8 +2,13 @@
   (:require [clojure.string :as str]
             [stella.numbers :as numbers]))
 
+(declare ^:private parse-expr)
+
+(def ^:private function-names
+  #{"sqrt" "sin" "cos" "tan" "ln" "exp"})
+
 (def ^:private token-pattern
-  #"[A-Za-z][A-Za-z0-9]*|-?\d+(?:\.\d+)?(?:\s*/\s*\d+)?|[+*]")
+  #"sqrt|sin|cos|tan|ln|exp|[A-Za-z][A-Za-z0-9]*|-?\d+(?:\.\d+)?(?:\s*/\s*\d+)?|[-+^*/()]")
 
 (defn- tokenize
   [formula]
@@ -16,9 +21,35 @@
         (throw (ex-info "invalid formula syntax" {:formula formula})))
       tokens)))
 
+(defn- function-token?
+  [token]
+  (contains? function-names token))
+
 (defn- stock-token?
   [token]
-  (re-matches #"[A-Za-z][A-Za-z0-9]*" token))
+  (and (re-matches #"[A-Za-z][A-Za-z0-9]*" token)
+       (not (function-token? token))))
+
+(def ^:private function-implementations
+  {"sqrt" Math/sqrt
+   "sin" Math/sin
+   "cos" Math/cos
+   "tan" Math/tan
+   "ln" Math/log
+   "exp" Math/exp})
+
+(defn- apply-function
+  [name value]
+  ((function-implementations name) value))
+
+(defn- parse-parenthesized
+  [tokens stock-value]
+  (when-not (= "(" (first tokens))
+    (throw (ex-info "expected opening parenthesis" {})))
+  (let [[value remaining] (parse-expr (rest tokens) stock-value)]
+    (when-not (= ")" (first remaining))
+      (throw (ex-info "missing closing parenthesis" {})))
+    [value (rest remaining)]))
 
 (defn- parse-factor
   [tokens stock-value]
@@ -28,22 +59,40 @@
       (nil? token)
       (throw (ex-info "unexpected end of formula" {}))
 
+      (= "(" token)
+      (parse-parenthesized tokens stock-value)
+
+      (function-token? token)
+      (let [[value remaining] (parse-parenthesized rest-tokens stock-value)]
+        [(apply-function token value) remaining])
+
       (stock-token? token)
       [(or (stock-value token)
-            (throw (ex-info "unknown stock" {:stock token})))
+           (throw (ex-info "unknown stock" {:stock token})))
        rest-tokens]
 
       :else
       [(numbers/parse-number token) rest-tokens])))
 
+(defn- parse-power
+  [tokens stock-value]
+  (let [[base tokens'] (parse-factor tokens stock-value)]
+    (if (= "^" (first tokens'))
+      (let [[exponent tokens''] (parse-factor (rest tokens') stock-value)]
+        [(Math/pow base exponent) tokens''])
+      [base tokens'])))
+
 (defn- parse-term
   [tokens stock-value]
   (loop [tokens tokens
-         [value tokens'] (parse-factor tokens stock-value)]
-    (if (= "*" (first tokens'))
-      (recur (rest tokens')
-             (let [[factor tokens''] (parse-factor (rest tokens') stock-value)]
-               [(* value factor) tokens'']))
+         [value tokens'] (parse-power tokens stock-value)]
+    (case (first tokens')
+      "*" (let [[factor tokens''] (parse-power (rest tokens') stock-value)]
+            (recur tokens'' [(* value factor) tokens'']))
+      "/" (let [[factor tokens''] (parse-power (rest tokens') stock-value)]
+            (when (zero? factor)
+              (throw (ex-info "division by zero" {})))
+            (recur tokens'' [(/ value factor) tokens'']))
       [value tokens'])))
 
 (defn- parse-expr
@@ -51,20 +100,31 @@
   (loop [tokens tokens
          [value tokens'] (parse-term tokens stock-value)]
     (cond
+      (= ")" (first tokens'))
+      [value tokens']
+
       (= "+" (first tokens'))
-      (recur (rest tokens')
-             (let [[term tokens''] (parse-term (rest tokens') stock-value)]
-               [(+ value term) tokens'']))
+      (let [[term tokens''] (parse-term (rest tokens') stock-value)]
+        (recur tokens'' [(+ value term) tokens'']))
+
+      (= "-" (first tokens'))
+      (let [[term tokens''] (parse-term (rest tokens') stock-value)]
+        (recur tokens'' [(- value term) tokens'']))
 
       (seq tokens')
       (throw (ex-info "unexpected tokens" {:tokens tokens'}))
 
       :else
-      value)))
+      [value tokens'])))
 
 (defn evaluate
   [formula stock-value]
-  (parse-expr (tokenize formula) stock-value))
+  (let [[value _] (parse-expr (tokenize formula) stock-value)]
+    value))
+
+(defn- formula-stocks
+  [formula]
+  (filter stock-token? (tokenize formula)))
 
 (defn valid-syntax?
   [formula]
@@ -74,12 +134,11 @@
     (catch Exception _ false)))
 
 (defn valid-for-stocks?
-  [formula stock-names]
+  [formula bound-stocks]
   (and (valid-syntax? formula)
-       (let [tokens (tokenize formula)
-             stocks (filter stock-token? tokens)]
-         (every? stock-names stocks))))
-
-;; clj-mutate-manifest-begin
-;; {:version 1, :tested-at "2026-06-30T11:22:31.689898-05:00", :module-hash "-532523093", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 3, :hash "282585715"} {:id "def/token-pattern", :kind "def", :line 5, :end-line 6, :hash "-1255527670"} {:id "defn-/tokenize", :kind "defn-", :line 8, :end-line 17, :hash "1599254320"} {:id "defn-/stock-token?", :kind "defn-", :line 19, :end-line 21, :hash "-1566555243"} {:id "defn-/parse-factor", :kind "defn-", :line 23, :end-line 37, :hash "1688837602"} {:id "defn-/parse-term", :kind "defn-", :line 39, :end-line 47, :hash "-253635061"} {:id "defn-/parse-expr", :kind "defn-", :line 49, :end-line 63, :hash "1787569189"} {:id "defn/evaluate", :kind "defn", :line 65, :end-line 67, :hash "1450074755"} {:id "defn/valid-syntax?", :kind "defn", :line 69, :end-line 74, :hash "-1478954409"} {:id "defn/valid-for-stocks?", :kind "defn", :line 76, :end-line 81, :hash "14519027"}]}
-;; clj-mutate-manifest-end
+       (let [stocks (set (formula-stocks formula))]
+         (and (every? bound-stocks stocks)
+              (try
+                (evaluate formula (constantly 0.0))
+                true
+                (catch Exception _ false))))))
--- test/stella/formula_test.clj
+++ test/stella/formula_test.clj
@@ -22,9 +22,22 @@
   (is (= 50.0 (formula/evaluate "Stock1 + Stock2"
                                 (stock-values "Stock1" 30.0 "Stock2" 20.0)))))
 
+(deftest evaluate-parentheses-and-division-test
+  (is (= 15.0 (formula/evaluate "(Stock1 + Stock2) * 0.1"
+                               (stock-values "Stock1" 100.0 "Stock2" 50.0))))
+  (is (= 30.0 (formula/evaluate "(Stock1 - Stock2) / 2"
+                               (stock-values "Stock1" 80.0 "Stock2" 20.0)))))
+
+(deftest evaluate-functions-and-exponent-test
+  (is (= 9.0 (formula/evaluate "Stock1 ^ 2" (stock-values "Stock1" 3.0))))
+  (is (= 2.0 (formula/evaluate "sqrt(Stock1)" (stock-values "Stock1" 4.0))))
+  (is (= 1.0 (formula/evaluate "cos(Stock1)" (stock-values "Stock1" 0.0)))))
+
 (deftest reject-invalid-syntax-test
   (is (false? (formula/valid-syntax? "Stock1 & 0.1"))))
 
 (deftest reject-unknown-stock-test
   (is (false? (formula/valid-for-stocks? "Missing * 2" #{"Stock1"})))
-  (is (true? (formula/valid-for-stocks? "Stock1 * 0.1" #{"Stock1"}))))
+  (is (false? (formula/valid-for-stocks? "Stock1 * 0.1" #{})))
+  (is (true? (formula/valid-for-stocks? "Stock1 * 0.1" #{"Stock1"})))
+  (is (true? (formula/valid-for-stocks? "5" #{}))))
